0218aac87bb25610a75247c6ae1980f189953b29
rocky123454321 <rockyjayducusin3@gmail.com>
2026-01-16 16:38:19 +0800

    Fix Prisma 7.x compatibility issues

--- server/server.ts
+++ server/server.ts
@@ -58,7 +58,7 @@ app.use(express.json({ limit: '50mb' }));
 
 // Health check route
 app.get('/', (req: Request, res: Response) => {
-  res.send('Server is Live!now');
+  res.send('Server is Live! now');
 });
 
 // Routes
