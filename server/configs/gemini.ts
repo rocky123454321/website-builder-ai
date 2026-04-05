ccb163e8db1f0399461cbb04294460c2cc7faa38
rocky Ducusin <rockyjayducusin3@gmail.com>
2026-04-05 17:14:47 +0800

    123

--- server/configs/gemini.ts
+++ server/configs/gemini.ts
@@ -7,7 +7,7 @@ if (!process.env.AI_API_KEY) {
 const genAI = new GoogleGenerativeAI(process.env.AI_API_KEY!);
 
 export const gemini = genAI.getGenerativeModel({
-  model: 'gemini-2.0-flash',
+  model: 'gemini-1.5-flash',
 });
 
 export default gemini;
